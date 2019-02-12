c71bfab0ca0d216b1c079e596c4800c3a5134a13
Ekaterina Kazakova <30477033+vrestles@users.noreply.github.com>
2019-02-12 08:43:18 +0300

    Files from game project

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Ekaterina Kazakova on 29.09.2018.
+//  Copyright © 2018 kazakova. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ BullsEye/BullsEye/AuthorViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by Ekaterina Kazakova on 29.09.2018.
+//  Copyright © 2018 kazakova. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
